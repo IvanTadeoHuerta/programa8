1acb8e0fa1d56a97a4b6bc99abbda561b34fc8c3
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-09-06 13:31:54 -0500

    Se agrego Vue-router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import EventBus from './plugins/event-bus'
-
+import Router from './routers.js'
 
 Vue.use(EventBus)
-
+Vue.use(VueRouter)
+const router = new VueRouter({ Router,  mode: 'history' })
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
---
+++ src/routers.js
@@ -0,0 +1,8 @@
+import FormularioInspecion from './components/FormularioInspeccion.vue'
+
+const routes = [
+                { path: '/consultar' , component: FormularioInspecion }
+            ]
+
+export default routes
+
