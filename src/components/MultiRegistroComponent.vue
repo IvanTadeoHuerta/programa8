a66b97729b850c4b37739d8bc25e367de023d7b2
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-09-12 17:44:34 -0500

    Template String de consulta de  todos los multiRegistros

--- src/components/MultiRegistroComponent.vue
+++ src/components/MultiRegistroComponent.vue
@@ -7,8 +7,8 @@
                         <slot name="header">Multiregistro no identificado</slot>
                     </div>                    
                 </div>
-                <div class="modal-body" style="max-height: 400px; overflow-y:auto;">
-                    <slot name="body">Opción no valida de multiregistro. Si el problema continúa contacte al administrador del sistema</slot>
+                <div class="modal-body" style="max-height: 400px; overflow-y:auto;">                    
+                    <slot name="body">Opción no valida de multiregistro. Si el problema continúa contacte al administrador del sistema</slot>      
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
--- src/modulos/ModuloRegistro.vue
+++ src/modulos/ModuloRegistro.vue
@@ -19,6 +19,7 @@
             <predios v-else-if="tipo == 'predios'"></predios>
             <inspectores v-else-if="tipo == 'inspectores'"></inspectores>
             <archivos v-else-if="tipo == 'archivos'"></archivos>
+            <div v-else>No se encontró  multiregistro. Contacte con el administrador del sistema</div>
        </template>
     </multiregistro>
   </div>
@@ -32,31 +33,255 @@ import Multiregistro from '../components/MultiRegistroComponent.vue'
 
 
 let Dependencias = {
-  template: '<div>A custom dependencias!</div>'
+  template: `<table class="table">
+                <thead>
+                    <tr>
+                        <th>Consecutivo</th>
+                        <th>Folio</th>
+                        <th>Dependencia</th>
+                        <th>Actualizar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <template v-if="dependencias.length > 0">
+                          <tr
+                              v-for="(dependencia, index ) in dependencias" 
+                              :key="dependencia.id" style="cursor:pointer">
+
+                                <td>{{ dependencia.consecutivo }}</td>
+                                <td>{{ dependencia.folio }}</td>
+                                <td>{{ dependencia.dependencia  }} </td>
+                                <td><button type="button" class="btn btn-success">Actualizar</button></td>
+                                <td><button type="button" class="btn btn-default">Eliminar</button></td>
+                          </tr>
+                    </template>
+                    <tr v-else>
+                        <td colspan="5" class="text-center">No hay registros</td>
+                    </tr>
+                </tbody>
+            </table>`,
+  data(){
+    return {
+      dependencias: []
+    }
+  }
 }
 
 let Vehiculos = {
-  template: '<div>A custom Vehiculos!</div>'
+  template: `<table class="table">
+                <thead>
+                    <tr>
+                        <th>Consecutivo</th>
+                        <th>Folio</th>
+                        <th>Dependencia</th>
+                        <th>Actualizar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   <template v-if="vehiculos.length > 0">
+                          <tr
+                              v-for="(vehiculo, index ) in vehiculos" 
+                              :key="vehiculo.id" style="cursor:pointer">
+
+                                <td>{{ vehiculo.consecutivo }}</td>
+                                <td>{{ vehiculo.folio }}</td>
+                                <td>{{ vehiculo.dependencia  }} </td>
+                                <td><button type="button" class="btn btn-success">Actualizar</button></td>
+                                <td><button type="button" class="btn btn-default">Eliminar</button></td>
+                          </tr>
+                    </template>
+                    <tr v-else>
+                        <td colspan="5" class="text-center">No hay registros</td>
+                    </tr>
+                </tbody>
+            </table>`,
+  data(){
+    return {
+      vehiculos: []
+    }
+  }
 }
 
 let Personas = {
-  template: '<div>A custom Personas!</div>'
+  template: `<table class="table">
+                <thead>
+                    <tr>
+                        <th>Consecutivo</th>
+                        <th>Folio</th>
+                        <th>Nombre del infractor</th>
+                        <th>Actualizar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   <template v-if="personas.length > 0">
+                          <tr
+                              v-for="(persona, index ) in personas" 
+                              :key="persona.id" style="cursor:pointer">
+
+                                <td>{{ persona.consecutivo }}</td>
+                                <td>{{ persona.folio }}</td>
+                                <td>{{ persona.nombre_infractor  }} </td>
+                                <td><button type="button" class="btn btn-success">Actualizar</button></td>
+                                <td><button type="button" class="btn btn-default">Eliminar</button></td>
+                          </tr>
+                    </template>
+                    <tr v-else>
+                        <td colspan="5" class="text-center">No hay registros</td>
+                    </tr>
+                </tbody>
+            </table>`,
+  data(){
+    return {
+      personas: []
+    }
+  }
 }
 
 let Bienes = {
-  template: '<div>A custom Bienes!</div>'
+  template:  `<table class="table">
+                <thead>
+                    <tr>
+                        <th>Consecutivo</th>
+                        <th>Folio</th>
+                        <th>Categoria del Bien y Producto</th>
+                        <th>Actualizar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                     <template v-if="bienes.length > 0">
+                          <tr
+                              v-for="(bienes, index ) in bienes" 
+                              :key="bienes.id" style="cursor:pointer">
+
+                                <td>{{ bienes.consecutivo }}</td>
+                                <td>{{ bienes.folio }}</td>
+                                <td>{{ bienes.categoria  }} </td>
+                                <td><button type="button" class="btn btn-success">Actualizar</button></td>
+                                <td><button type="button" class="btn btn-default">Eliminar</button></td>
+                          </tr>
+                    </template>
+                    <tr v-else>
+                        <td colspan="5" class="text-center">No hay registros</td>
+                    </tr>
+                </tbody>
+            </table>`,
+  data(){
+    return {
+      bienes: []
+    }
+  }
 }
 
 let Predios = {
-  template: '<div>A custom Predios!</div>'
+  template: `<table class="table">
+                <thead>
+                    <tr>
+                        <th>Consecutivo</th>
+                        <th>Folio</th>
+                        <th>Clave única de predio</th>
+                        <th>Actualizar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                     <template v-if="predios.length > 0">
+                          <tr
+                              v-for="(predio, index ) in predios" 
+                              :key="predio.id" style="cursor:pointer">
+
+                                <td>{{ predio.consecutivo }}</td>
+                                <td>{{ predio.folio }}</td>
+                                <td>{{ predio.cup  }} </td>
+                                <td><button type="button" class="btn btn-success">Actualizar</button></td>
+                                <td><button type="button" class="btn btn-default">Eliminar</button></td>
+                          </tr>
+                    </template>
+                    <tr v-else>
+                        <td colspan="5" class="text-center">No hay registros</td>
+                    </tr>
+                </tbody>
+            </table>`,
+  data(){
+    return {
+      predios: []
+    }
+  }
 }
 
 let Inspectores = {
-  template: '<div>A custom Inspectores!</div>'
+  template: `<table class="table">
+                <thead>
+                    <tr>
+                        <th>Consecutivo</th>
+                        <th>Folio</th>
+                        <th>Inspector Forestal</th>
+                        <th>Actualizar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <template v-if="inspectores.length > 0">
+                          <tr
+                              v-for="(inspectores, index ) in inspectores" 
+                              :key="inspectores.id" style="cursor:pointer">
+
+                                <td>{{ inspectores.consecutivo }}</td>
+                                <td>{{ inspectores.folio }}</td>
+                                <td>{{ inspectores.inspector  }} </td>
+                                <td><button type="button" class="btn btn-success">Actualizar</button></td>
+                                <td><button type="button" class="btn btn-default">Eliminar</button></td>
+                          </tr>
+                    </template>
+                    <tr v-else>
+                        <td colspan="5" class="text-center">No hay registros</td>
+                    </tr>
+                </tbody>
+            </table>`,
+  data(){
+    return {
+      inspectores: []
+    }
+  }
 }
 
 let Archivos = {
-  template: '<div>A custom Archivos!</div>'
+  template: `<table class="table">
+                <thead>
+                    <tr>
+                        <th>Consecutivo</th>
+                        <th>Folio</th>
+                        <th>Campo Asociado</th>
+                        <th>Actualizar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <template v-if="archivos.length > 0">
+                          <tr
+                              v-for="(archivo, index ) in archivos" 
+                              :key="archivo.id" style="cursor:pointer">
+
+                                <td>{{ archivo.consecutivo }}</td>
+                                <td>{{ archivo.folio }}</td>
+                                <td>{{ archivo.campo  }} </td>
+                                <td><button type="button" class="btn btn-success">Actualizar</button></td>
+                                <td><button type="button" class="btn btn-default">Eliminar</button></td>
+                          </tr>
+                    </template>
+                    <tr v-else>
+                        <td colspan="5" class="text-center">No hay registros</td>
+                    </tr>
+                </tbody>
+            </table>`,
+  data(){
+    return {
+      archivos: []
+    }
+  }
 }
 
 export default {
