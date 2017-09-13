df60793783dbf402dc6b65ae15a277eab7ab84dc
Ramiro Luna <rlunaaragon@gmail.com>
2017-09-13 14:50:50 -0500

    Se agregó el archivo PersonasAseguradascomponent.vue

---
+++ src/components/PersonasAseguradasComponent.vue
@@ -0,0 +1,72 @@
+<template>
+	<form id="personasAseguradasInfraccionadas">
+    	<div class="col-lg-12">
+    		<div class="form-group">
+    			<div class="row">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+    					<label>Consecutivo</label>
+    					<input type="text" name="consecutivo" class="form-control" value="${this[posicion].consecutivo}">
+    				</div>
+
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+    					<label>Folio</label>
+    					<input type="text" name="folio" class="form-control" value="${this[posicion].folio}">
+    				</div>
+    			</div>    			
+    		</div>
+
+    		<div class="form-group">
+    			<div class="row">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+    					<label>Nombre del Infractor</label>
+    					<input type="text" name="nombre" class="form-control">
+    				</div>
+
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+    					<label>Sexo del Infractor</label>
+    					<select name="sexo" class="form-control">
+    						<option value="-1">Seleccione una opción...</option>
+    					</select>
+    				</div>
+    			</div>
+    		</div>
+
+    		<div class="form-group">
+    			<div class="row">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+    					<label>Edad del Infractor</label>
+    					<input type="text" name="edad" class="form-control">
+    				</div>
+
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+    					<label>Domicilio del Infractor</label>
+    					<input type="text" name="domicilio" class="form-control">
+    				</div>
+    			</div>
+    		</div>
+
+    		<div class="form-group">
+    			<div class="row">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+    					<label>Estatus de la persona</label>
+    					<select name="estatus" class="form-control">
+    						<option value="-1">Seleccione una opción...</option>
+    					</select>
+    				</div>
+    			</div>
+    		</div>
+
+    		<div class="form-group">
+                <div class="row">
+                    <br>
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <button type="button" class="btn btn-default btn-block">Cancelar</button>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <button type="button" class="btn btn-success btn-block">Aceptar</button>
+                    </div>
+                </div>
+            </div>
+    	</div>
+    </form>
+</template>
