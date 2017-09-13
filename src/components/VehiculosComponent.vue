cc34e964d18ce703585e1fd50ad6dda38be91187
Ramiro Luna <rlunaaragon@gmail.com>
2017-09-13 12:10:22 -0500

    Se agregó formulario de vehículo

--- src/components/VehiculosComponent.vue
+++ src/components/VehiculosComponent.vue
@@ -1,3 +1,27 @@
 <template>
-  
+ 	<form id="vehiculosAsegurados">
+    	<div class="col-lg-12">
+    		<div class="form-group">
+    			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    				<input type="hidden" name="consecutivo" class="form-control" value="${this[posicion].consecutivo}">
+    			</div>
+
+    			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    				<input type="hidden" name="folio" class="form-control" value="${this[posicion].folio}">
+    			</div>
+    		</div>
+
+    		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    			<label>Dependencia</label>
+    			<select name="dependencia" class="form-control">
+    				<option value="-1">Seleccione una opción...</option>
+    			</select>
+    		</div>
+
+    		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    			<label>Cantidad</label>
+    			<input type="text" name="cantidad" class="form-control">
+    		</div>
+    	</div>
+    </form>
 </template>
