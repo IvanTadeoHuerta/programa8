34a8f7708c414b1e9dd0d12770a00f975382d6aa
Ramiro Luna <rlunaaragon@gmail.com>
2017-09-13 14:16:10 -0500

    Se modificó el archivo PrediosComponent.vue y VehiculosComponent.vue

--- src/components/PrediosComponent.vue
+++ src/components/PrediosComponent.vue
@@ -3,24 +3,26 @@
     	<div class="col-lg-12">
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
-    					<input type="hidden" name="consecutivo" class="form-control" value="${this[posicion].consecutivo}">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+                        <label>Consecutivo</label>
+    					<input type="text" name="consecutivo" class="form-control" value="${this[posicion].consecutivo}">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
-    					<input type="hidden" name="folio" class="form-control" value="${this[posicion].folio}">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
+                        <label>Folio</label>
+    					<input type="text" name="folio" class="form-control" value="${this[posicion].folio}">
     				</div>
     			</div>
     		</div>
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Clave única de predio</label>
     					<input type="text" name="clave_unica_predio" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Cve_sedemex</label>
     					<input type="text" name="cve_sedemex" class="form-control">
     				</div>
@@ -29,12 +31,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Campo Trabajo</label>
     					<input type="text" name="campo_trabajo" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Nombre del Predio</label>
     					<input type="text" name="nombre_del_predio" class="form-control">
     				</div>
@@ -43,12 +45,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Cve_Tenencia</label>
     					<input type="text" name="cve_tenencia" class="form-control">
     				</div>  
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Descripción de como llegar al predio</label>
     					<textarea name="descripcion_llegar_predio" class="form-control" rows="3" cols="5"></textarea>
     				</div> 				
@@ -57,12 +59,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Latitud(UTM)</label>
     					<input type="text" name="latitud_utm" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Longitud(UTM)</label>
     					<input type="text" name="longitud_utm" class="form-control">
     				</div>
@@ -71,12 +73,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Superficie total(Ha)</label>
     					<input type="text" name="superficie_total_ha" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Superficie cartográfica(Ha)</label>
     					<input type="text" name="superficie_cartografica_ha" class="form-control">
     				</div>
@@ -85,12 +87,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Superficie arbolada(Ha)</label>
     					<input type="text" name="superficie_arbolada_ha" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Superficie otros usos(Ha)</label>
     					<input type="text" name="superficie_otros_usos_ha" class="form-control">
     				</div>
@@ -99,12 +101,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Microcuenca(subcuenca especifica)</label>
     					<input type="text" name="microcuenca_subcuenca_especifica" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Área natural protegida</label>
     					<select name="area_natural_protegida" class="form-control">
     						<option value="-1">Seleccione una opción...</option>
@@ -115,12 +117,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Status del predio</label>
     					<input type="text" name="status_predio" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Observaciones del predio</label>
     					<textarea name="observaciones_predio" class="form-control" rows="3" cols="5"></textarea>
     				</div>
@@ -129,12 +131,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Propietario o Representante legal</label>
     					<input type="text" name="propietario_o_representante_legal" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>Polígonos del predio</label>
     					<input type="text" name="poligonos_del_predio" class="form-control">
     				</div>
@@ -143,12 +145,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>CLAVE ESTATAL</label>
     					<input type="text" name="clave_estatal" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>CVE_REGFTAL</label>
     					<input type="text" name="cve_regftal" class="form-control">
     				</div>
@@ -157,12 +159,12 @@
 
     		<div class="form-group">
     			<div class="row">
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>CVE_MUN</label>
     					<input type="text" name="cve_num" class="form-control">
     				</div>
 
-    				<div class="col-md-6 col-sm-6 col-xs-12">
+    				<div class="col-md-6 col-sm-12 col-xs-12">
     					<label>CVE_LOC</label>
     					<input type="text" name="cve_loctext" class="form-control">
     				</div>
@@ -170,12 +172,16 @@
     		</div>
 
     		<div class="form-group">
-    			<div class="row">
-    				<div class="col-md-offset-5 col-sm-offset-5 col-xs-offset-4">
-    					<button type="submit" class="btn btn-success" id="aceptar">Aceptar</button>
-    				</div>
-    			</div>
-    		</div>
+                <div class="row">
+                    <br>
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <button type="button" class="btn btn-default btn-block">Cancelar</button>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <button type="button" class="btn btn-success btn-block">Aceptar</button>
+                    </div>
+                </div>
+            </div>
     	</div>
     </form>
 </template>
--- src/components/VehiculosComponent.vue
+++ src/components/VehiculosComponent.vue
@@ -2,26 +2,46 @@
  	<form id="vehiculosAsegurados">
     	<div class="col-lg-12">
     		<div class="form-group">
-    			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-    				<input type="hidden" name="consecutivo" class="form-control" value="${this[posicion].consecutivo}">
-    			</div>
+                <div class="row">
+        			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <label>Consecutivo</label>
+        				<input type="text" name="consecutivo" class="form-control" value="${this[posicion].consecutivo}">
+        			</div>
 
-    			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-    				<input type="hidden" name="folio" class="form-control" value="${this[posicion].folio}">
-    			</div>
+        			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <label>Folio</label>
+        				<input type="text" name="folio" class="form-control" value="${this[posicion].folio}">
+        			</div>
+                </div>
     		</div>
 
-    		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-    			<label>Dependencia</label>
-    			<select name="dependencia" class="form-control">
-    				<option value="-1">Seleccione una opción...</option>
-    			</select>
-    		</div>
+    		<div class="form-group">
+                <div class="row">
+                    <div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+                        <label>Dependencia</label>
+                        <select name="dependencia" class="form-control">
+                            <option value="-1">Seleccione una opción...</option>
+                        </select>
+                    </div>
 
-    		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-    			<label>Cantidad</label>
-    			<input type="text" name="cantidad" class="form-control">
-    		</div>
+                    <div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+                        <label>Cantidad</label>
+                        <input type="text" name="cantidad" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <br>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <button type="button" class="btn btn-default btn-block">Cancelar</button>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <button type="button" class="btn btn-success btn-block">Aceptar</button>
+                    </div>
+                </div>
+            </div>
     	</div>
     </form>
 </template>
